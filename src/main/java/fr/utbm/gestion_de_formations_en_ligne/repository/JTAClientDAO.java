298dff6c32ab85747952e40c2b02111da9d95013
Ali <alitriki2@gmail.com>
2016-06-05 23:17:59 +0200

    meh (demi solution)

--- src/main/java/fr/utbm/gestion_de_formations_en_ligne/repository/JTAClientDAO.java
+++ src/main/java/fr/utbm/gestion_de_formations_en_ligne/repository/JTAClientDAO.java
@@ -14,7 +14,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.transaction.Transaction;
+import org.hibernate.Transaction;
 import org.hibernate.Session;
 
 /**
@@ -22,42 +22,40 @@ import org.hibernate.Session;
  * @author Ali
  */
 public class JTAClientDAO {
-    
-        public void insertClientJta(Client client) throws Exception {
-//        Session session = HibernateUtil.getSessionFactory().openSession();
-//        session.beginTransaction();
-//        session.persist(client);
-//        session.getTransaction().commit();
+
+    public void insertClientJta(Client client) throws Exception {
+
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = null;
+
+        try {
+            System.out.println("test0 ");
+           tx = session.beginTransaction();
             
-          Session session = HibernateUtil.getSessionFactory().openSession();
-          Transaction tx = null;
-          
-          try{
-              System.out.println("test0 ");
-              tx =  (Transaction) session.beginTransaction();
-              System.out.println("test0.5 ");
-              Log log = new Log();
-              DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
-              Date date = new Date();
-              System.out.println("test1 ");
-              log.setDates(date);
-              log.setStudent(client.getFirstname()+" "+client.getLastname());
-              System.out.println("test2 ");
-              session.persist(log);
-              System.out.println("test3 ");
-              //Mettre l'exception
-              session.persist(client);
-              System.out.println("test4 ");
-              tx.commit();
-          }
-          catch(Exception e){
-              System.out.println("test5 ");
-              Logger.getLogger(ClientService.class.getName()).log(Level.SEVERE, null, e);
-//              tx.rollback();
-              
-          }
-          finally{
-              session.close();
-          }
+            System.out.println("test0.5 ");
+            Log log = new Log();
+            DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+            Date date = new Date();
+            System.out.println("test1 ");
+            log.setDates(date);
+            log.setStudent(client.getFirstname() + " " + client.getLastname());
+            System.out.println("test2 ");
+            session.persist(log);
+            System.out.println("test3 ");
+            
+            /** Exception */
+            int x=5/0;
+            
+            session.persist(client);
+            System.out.println("test4 ");
+            tx.commit();
+        } catch (Exception e) {
+            System.out.println("test5 ");
+            Logger.getLogger(ClientService.class.getName()).log(Level.SEVERE, null, e);
+             tx.rollback();
+
+        } finally {
+            session.close();
+        }
     }
 }
